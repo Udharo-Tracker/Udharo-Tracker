187a7102041c3db577ba43393f86b2931b6f7ebf
Manisha Magar <magarmanisha248@gmail.com>
2026-04-30 22:43:47 +0545

    fix: fixed owner

--- backend/apps/shops/models.py
+++ backend/apps/shops/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+from django.conf import settings
 
 # Create your models here.
 class Shop(models.Model):
-    owner = models.ForeignKey("account.User", on_delete=models.CASCADE, related_name='shop')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='shop')
     name = models.CharField(max_length=255)
     phone = models.CharField(max_length=15, blank=True)
     address = models.TextField(blank=True)
