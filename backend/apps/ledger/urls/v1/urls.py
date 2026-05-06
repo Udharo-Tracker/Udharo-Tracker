e03c91e0465a222327ebe4a50f1f2129c2095294
Manisha Magar <magarmanisha248@gmail.com>
2026-05-06 22:31:07 +0545

    feat: added ledgerSummery in url

--- backend/apps/ledger/urls/v1/urls.py
+++ backend/apps/ledger/urls/v1/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 from apps.ledger.api.v1.payment import PaymentViewSet
 from apps.ledger.api.v1.udharoEntry import UdharoEntryViewSet
 from apps.ledger.api.v1.customerStatement import CustomerStatementView
+from apps.ledger.api.v1.ledgerSummery import LedgerSummaryView
 
 router = DefaultRouter()
 router.register(r'udharo', UdharoEntryViewSet, basename='udharo')
@@ -16,5 +17,10 @@ urlpatterns = [
     "customers/<uuid:customer_id>/statement/",
     CustomerStatementView.as_view(),
     name="customer-statement",
+    ),
+    path(
+    "/ledger/summary/",
+    LedgerSummaryView.as_view(),
+    name="ledger-summary",
     ),
 ]
