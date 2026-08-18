1d930c9c0fb1f98631dabfcebc280ebbc0edebc7
Manisha Magar <magarmanisha248@gmail.com>
2026-08-18 21:23:31 +0545

    refactor(ledger): export recalculate_all_credit_scores from tasks package

--- backend/apps/ledger/tasks/__init__.py
+++ backend/apps/ledger/tasks/__init__.py
@@ -1,3 +1,4 @@
 from .reminderLog import send_daily_reminders
+from .creditScore import recalculate_all_credit_scores
 
-__all__ = ['send_daily_reminders']
+__all__ = ["send_daily_reminders", "recalculate_all_credit_scores"]
